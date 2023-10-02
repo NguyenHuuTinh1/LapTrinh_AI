8068895f3713213e6942dad199ae616b1b19c6f1
NguyenHuuTinh1 <21130202@st.hcmuaf.edu.vn>
2023-10-02 20:55:43 +0700

    up task1

---
+++ Java_NhapMon_AI/src/chapter2/agent_ABCD/AgentProgram.java
@@ -0,0 +1,17 @@
+package chapter2.agent_ABCD;
+
+import chapter2.agent_ABCD.Environment.LocationState;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		// TODO
+		if (p.getLocationState() == LocationState.DIRTY) {
+			return Environment.SUCK_DIRT;
+		} else if (p.getAgentLocation() == Environment.LOCATION_A) {
+			return Environment.MOVE_RIGHT;
+		} else {
+			return Environment.MOVE_LEFT;
+		}
+	}
+}
